2f7b99fac865f2a01b993119dc0bf967d19e73da
viraj <reathia.viraj@gmail.com>
2021-10-14 01:37:21 +0530

    Creating signup route

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
-
-
 const app = express();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.pmdco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
+  
+  mongoose.connection.on("error", function(error) {
+    console.log(error)
+  })
+  
+  mongoose.connection.on("open", function() {
+    console.log("Connected to MongoDB database.")
+  })
 
-const mongoose = require("mongoose");
+  require('./models/user')
+  app.use(express.json())
+  app.use(require('./routes/auth'))
 
 
 app.listen(8000, ()=>{
